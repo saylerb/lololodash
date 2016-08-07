b71a91357ef774466e247d296fb1c858c5a14601
Brian Sayler <sayler.b@gmail.com>
2016-08-07 14:14:12 -0600

    add some helper methods

--- everyone_is_min.js
+++ everyone_is_min.js
@@ -1,20 +1,31 @@
 var _ = require("lodash");
 
 var SortCitiesByTemp = function(cities) {
-  
   var result = { hot: [], warm: [] }
 
   for (var city in cities) {
     temps = cities[city]
 
-    if (_.every(temps, temp => temp > 19)) {
+    if (cityIsHot(temps)) {
       result.hot.push(city)
-    } else if (_.some(temps, temp => temp > 19)) {
+    } else if (cityIsWarm(temps)) {
       result.warm.push(city)
     }
   }
-
   return result
 }
+
+function isTempHot(temp) {
+  return temp > 19;
+}
+
+function cityIsHot(temps) {
+  return _.every(temps, isTempHot)
+}
+
+function cityIsWarm(temps) {
+  return _.some(temps, isTempHot)
+}
+
  
 module.exports = SortCitiesByTemp;
