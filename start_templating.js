9992b9b78b40a3b307ac491ce127ded4e183ee0d
Brian Sayler <sayler.b@gmail.com>
2016-08-08 06:29:20 -0600

    refactor template

--- start_templating.js
+++ start_templating.js
@@ -1,9 +1,10 @@
 var _ = require("lodash");
 
-var templating = function(user) {
-  return _.template("Hello <%= name %> (logins: <%= logins %>)")(
-    { name: user.name, logins: _.size(user.login) }
-  )
+var templating = function(user_data) {
+
+  user_template = "Hello <%= name %> (logins: <%= _.size(login) %>)"
+
+  return _.template(user_template)(user_data)
 }
 
 module.exports = templating
